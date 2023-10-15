22c125679f66adcfc777b0482a0757b62f746d12
2022569kellen <2022569@student.cct.ie>
2023-10-15 14:23:18 +0100

    Initial commit

---
+++ src/occhomerwork2/Tesla.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package occhomerwork2;
+
+/**
+ *
+ * @author 35383
+ */
+public class Tesla extends Car{
+
+    @Override
+    public void run() {
+        System.out.println("The Tesla is running too much");
+        
+    }
+
+   
+    
+}
---
+++ src/occhomerwork2/thingsofcar.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package occhomerwork2;
+
+/**
+ *
+ * @author 35383
+ */
+public interface thingsofcar {
+    public void speedup(); 
+  
+}
